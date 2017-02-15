331c09231017750651c50fa258bea858f6bd271f
William Riley <wrileystl@gmail.com>
2017-02-15 12:38:07 -0600

    added option for providing command

--- runit.sh
+++ runit.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-ansible -i my_hosts linux --vault-password-file=~/.vault_pass -a "uptime"
+CMD="uptime"
+
+if [ -n "$1" ]; then
+    CMD="$1"
+fi
+
+ansible -i my_hosts linux --vault-password-file=~/.vault_pass -a "${CMD}"
